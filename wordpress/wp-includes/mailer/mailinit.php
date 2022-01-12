bc29d661b0c6e2469a90de496177366e1f44102e
Kuntz <nkuntz@scarlet.be>
2022-01-12 01:20:01 +0100

    Nuke and start again

--- wordpress/wp-includes/mailer/mailinit.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-require_once 'vendor/autoload.php'; //Input packet for swift_mailer
-$transport = (new Swift_SmtpTransport('localhost', 25)) //test 465
-->setUsername('magellan') // a modifier manuellement
-->setPassword('Magellanthenavigateur')  // a modifier manuellement
-;
-
